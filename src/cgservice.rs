dab153d8a76e6f6f306be8861a9be6a58d32cdc9
tyler komoroske <tjkomor@clemson.edu>
2021-12-30 14:18:55 -0700

    add get all currencies fn for cg service

--- src/cgservice.rs
+++ src/cgservice.rs
@@ -14,3 +14,19 @@ pub fn get_token(token: &str) -> Result<(), isahc::Error> {
 
     Ok(())
 }
+
+pub fn get_all_currencies() -> Result<(), isahc::Error> {
+    let url = "https://api.coingecko.com/api/v3/simple/supported_vs_currencies";
+    println!("{}", &url);
+    let mut response = isahc::get(url)?;
+
+    // Print some basic info about the response to standard output.
+    println!("Status: {}", response.status());
+    println!("Headers: {:#?}", response.headers());
+
+    // Read the response body as text into a string and print it.
+    print!("{}", response.text()?);
+
+    Ok(())
+}
+
